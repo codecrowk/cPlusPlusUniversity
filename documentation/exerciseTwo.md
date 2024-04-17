9dbc36a8bbddb0f3179bb27b180538d1895d9e82
dannyKental <handres41@outlook.com>
2024-04-17 09:31:48 -0500

    Making get older man

---
+++ documentation/exerciseTwo.md
@@ -0,0 +1,9 @@
+Ejercicio dos
+
+Para una cantidad desconocida de registros, se lee: nombre, edad, sexo, estado civil(1=soltero, 2=casado, 3= unión libre, 4=viudo), carrera (1=sistemas, 2=programación, 3=mantenimiento, 4=diseño).
+      Elabore un diagrama que encuentre e imprima:
+Nombre de la mujer de sistemas más joven.
+Nombre del hombre más viejo en sistemas.
+Promedio de edad de las personas de programación que son casadas
+Porcentaje que representan los menores de edad que estudian mantenimiento respecto a todas las personas de mantenimiento.
+Cuantas personas de diseño son mujeres mayores de edad.
--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,15 +1,127 @@
 #include <iostream>
+#include <string>
 #include <vector> 
+#include <format>
 
 using namespace std;
 
-// vector for store information
-vector< vector<string>> info = {
-  {"Jose", "Mario"},
-  {"22", "40"},
-  {"male", "female"},
-  {"single", "married"},
-  {"programmer", "Doctor"}
+class Users 
+{
+  public:
+    string Name;
+    string Age;
+    string Gender;
+    string CivilStatus;
+    string Carrer;
+};
+
+class UsersController
+{
+  private:
+    vector <Users> dataBase;
+    string vectorInfoPrint = "Name | Age | Gender | Carrer \n";
+  public:
+    void addNewUser (Users user)
+    {
+      dataBase.push_back(user);
+    }
+
+    void printUsers ()
+    {
+      // Print vector information
+      cout << vectorInfoPrint; 
+      for (int i = 0; i < dataBase.size(); i++)
+      {
+        Users user = dataBase[i];
+        cout << user.Name << " " << user.Age << " " << user.Gender << " " << user.Carrer << "\n";
+      }
+    }
+
+    void youngerSystemWoman ()
+    {
+      int youngerAge = 200;
+      cout << vectorInfoPrint; 
+
+      Users youngerWoman;
+
+      for (int i = 0; i < dataBase.size(); i++)
+      {
+        Users user = dataBase[i];
+        // Validate information about user
+        if (user.Gender == "Female"
+          && user.Carrer == "System engineer"
+          && stoi(user.Age) < youngerAge)
+        {
+          youngerWoman = user;
+        }
+      }
+
+      try
+      {
+        cout << youngerWoman.Name << " " << youngerWoman.Age << " " << youngerWoman.Gender << " " << youngerWoman.Carrer << "\n";
+        // if (youngerWoman.Gender == "") throw new exception;
+      }
+      catch(const exception& e)
+      {
+        cout << "Not founded user";
+      }
+    }
+
+    void olderSystemMan ()
+    {
+      int youngerAge = 0;
+      cout << vectorInfoPrint; 
+
+      Users olderMan;
+
+      for (int i = 0; i < dataBase.size(); i++)
+      {
+        Users user = dataBase[i];
+        // Validate information about user
+        if (user.Gender == "Male"
+          && user.Carrer == "System engineer"
+          && stoi(user.Age) > youngerAge)
+        {
+          olderMan = user;
+        }
+      }
+
+      try
+      {
+        cout << olderMan.Name << " " << olderMan.Age << " " << olderMan.Gender << " " << olderMan.Carrer << "\n";
+        // if (youngerWoman.Gender == "") throw new exception;
+      }
+      catch(const exception& e)
+      {
+        cout << "Not founded user";
+      }
+      
+    }
+};
+
+class DataConvertor
+{
+  private:
+    string Gender[2] = {"Male", "Female"};
+    string CivilStatus[4] = {"Single", "Married", "Widow", "No married"};
+    string Carrers[4] = {"System engineer", "Programmer", "Support", "Ux and Ui designer"};
+  
+  public:
+    string GetGender(int position)
+    {
+      return Gender[position];
+    }
+
+
+    string GetCarrer(int position)
+    {
+      return Carrers[position];
+    }
+
+    string GetCivilStatus(int position)
+    {
+      return CivilStatus[position];
+    }
 };
 
 /*
@@ -20,53 +132,57 @@ NOTES:
 */
 
 int main() {
-  // Global varibles
   int numberOfRegisters;
+  string carrer,
+    gender,
+    civilStatus;
 
-  // Print vector information
-  for (int col = 0; col < info.size(); col++)
-  {
-    for (int item = 0; item < info[0].size(); item++)
-    {
-      cout << info[0][item] << " ";
-      cout << info[1][item] << " ";
-      cout << info[2][item] << " \n";
-    }
-  }
+  UsersController userControll;
+  DataConvertor dataConvetor;
+  Users user;
 
   // Spaggeti code, disgustin
   cout << "Enter how many people you want to register: ";
   cin >> numberOfRegisters;
 
-
   system("cls"); 
-
 
   for (int i = 0; i < numberOfRegisters; i++) 
   {
     cout << "Enter your name: ";
-    cin >> info[0][i];
+    cin >> user.Name;
 
     cout << "Enter your age: ";
-    cin >> info[1][i];
+    cin >> user.Age;
 
     cout << "Enter your gender: ";
-    cin >> info[2][i];
+    cout << "Enter your gender: \n"  
+    << "1. man\n"
+    << "2. women: ";
+    cin >> gender;
+    // remember that array start at 0 position
+    user.Gender = dataConvetor.GetGender(stoi(gender) -1); 
 
     cout << "Enter your civil status: \n"  
     << "1. single\n"
     << "2. married\n"
     << "3. widow\n"
     << "4. no married: ";
-    cin >> info[3][i];
+    cin >> civilStatus;
+    user.CivilStatus = dataConvetor.GetCivilStatus(stoi(civilStatus) -1);
 
     cout << "Enter your carrer: \n"
     << "1. System engineer\n"
     << "2. Programmer\n"
     << "3. Support\n"
     << "4. Ux and Ui designer: ";
-    cin >> info[4][i];
+    cin >> carrer;
+    user.Carrer = dataConvetor.GetCarrer(stoi(carrer) -1);
+    userControll.addNewUser(user);
   }
+
+  // userControll.printUsers();
+  userControll.youngerSystemWoman();
   return 0;
 }
 
@@ -78,4 +194,6 @@ https://www.geeksforgeeks.org/vector-in-cpp-stl/
 https://www.geeksforgeeks.org/2d-vector-in-cpp-with-user-defined-size/
 
 https://www.geeksforgeeks.org/how-to-clear-console-in-cpp/
+
+https://www.studyplan.dev/intro-to-programming/string-interpolation
 */
